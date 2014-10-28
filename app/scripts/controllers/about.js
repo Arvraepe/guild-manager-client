42a7d3caafc180b356e3337c82e29776b0dc138d
Arne Van Raepenbusch <arvraepe@gmail.com>
2014-10-28 16:51:51 +0100

    yo scaffolding angular bootstrap code

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name guildManagementApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the guildManagementApp
+ */
+angular.module('guildManagementApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name guildManagementApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the guildManagementApp
+ */
+angular.module('guildManagementApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
